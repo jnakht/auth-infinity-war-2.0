0195c70bfe77b8dc8c0d83285bbc72a90e6cd1ff
jnakht <jisan.cse.cu@gmail.com>
2025-03-27 12:02:44 +0600

    user Name, Email, Password got from Register form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,36 @@
 
 const Register = () => {
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const userName = e.target.name.value;
+        const userEmail = e.target.email.value;
+        const userPassword = e.target.password.value;
+        console.log(userName, userEmail, userPassword);
+    }
     return (
-        <div>
-            
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col ">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={handleRegister}>
+                    <div className="card-body">
+                        <fieldset className="fieldset">
+                            <label className="fieldset-label">Name</label>
+                            <input type="text" name="name" className="input" placeholder="Name" />
+                            <label className="fieldset-label">Email</label>
+                            <input type="email" name="email" className="input" placeholder="Email" />
+                            <label className="fieldset-label">Password</label>
+                            <input type="password" name="password" className="input" placeholder="Password" />
+                            <div><a className="link link-hover">Forgot password?</a></div>
+                            <button className="btn btn-primary mt-4">Login</button>
+                        </fieldset>
+                    </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
